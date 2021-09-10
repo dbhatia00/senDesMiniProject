d0feb5c31f5b05e6090ec8bbec2f685133c80cb3
Thomas Scrivanich <73702777+tscrivanich27@users.noreply.github.com>
2021-09-10 12:57:23 -0400

    Updated App.js to include Gmail sign-in feature

--- App.js
+++ App.js
@@ -1,48 +1,130 @@
-//adapted from https://docs.expo.dev/versions/latest/sdk/bar-code-scanner/
-
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+//Expo Modules
+import React, { useState, useEffect } from "react"
+import { StyleSheet, Text, View, Image, Button } from "react-native"
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Google from "expo-google-app-auth"
 
-
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+export default class App extends React.Component {
+  //Constructor for managing different pages and signedIn state
+  constructor(props) {
+    super(props)
+    //State initialization
+    this.state = {
+      signedIn: false,
+    }
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+  //Function to handle signedIn state
+  signIn = async () => {
+    try {
+      const result = await Google.logInAsync({
+        //Credential for iOS clients
+        //Sign-in is only possible on iOS
+        iosClientId: "774439232289-l87u84h2b2qgqtdf2sfj83drk6nmvsf7.apps.googleusercontent.com",
+        scopes: ["profile", "email"]
+      })
+      //If the sign-in is successful...the state is changed to true
+      if (result.type === "success") {
+        this.setState({
+          //State changed to true
+          signedIn: true,
+        })
+      //Situation where the user exits sign-in page
+      } else {
+        console.log("cancelled")
+      }
+    //Situation where error occurs during sign-in
+    } catch (e) {
+      console.log("error", e)
+    }
   }
 
-  return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
-  );
+  render() {
+    return (
+      <View style={barcode_styles.barcode_view}>
+        {this.state.signedIn ? (
+          <BarcodePage/>
+        ) : (
+          <LoginPage signIn={this.signIn} />
+        )}
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
+//Login Page layout: text header and a button to begin sign-in process
+const LoginPage = props => {
+  return (
+    <View style={login_styles.login_view}>
+      <Text style={login_styles.header}>Sign In With Google</Text>
+      <Button title="Click to Authenticate" onPress={() => props.signIn()} />
+    </View>
+  )
+}
+
+//Barcode Page layout: notifies user of camera access status, full camera view, notifies user of successful scan
+const BarcodePage = props => {
+  const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+      (async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+      setScanned(true);
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={barcode_styles.barcode_view}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+        </View>
+    );
+}
+
+//Styles for login page and barcode page
+
+//Barcode page style
+const barcode_styles = StyleSheet.create({
+  barcode_view: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
+  }
+})
+
+//Login Page style
+const login_styles = StyleSheet.create({
+  login_view: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  header: {
+    fontSize: 25
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150
+  }
+})
